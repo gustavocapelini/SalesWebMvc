373a2487efc9fb7b3704522a930412dc548af5e3
gustavocapelini <gustavo.capelini8@gmail.com>
2020-08-11 18:59:44 -0300

    sellerservice and basic findall

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 
+using SalesWebMvc.Models.Services;
+
 namespace SalesWebMvc
 {
     public class Startup
@@ -40,6 +42,7 @@ namespace SalesWebMvc
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"),builder
                     => builder.MigrationsAssembly("SalesWebMvc")));
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
