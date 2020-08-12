36d0144c1a6d0f8c4f0feef3a9892d5312208700
gustavocapelini <gustavo.capelini8@gmail.com>
2020-08-11 23:09:33 -0300

    sellerformviewmodel and department select component

--- SalesWebMvc/Models/Services/SellerService.cs
+++ SalesWebMvc/Models/Services/SellerService.cs
@@ -20,7 +20,7 @@ namespace SalesWebMvc.Models.Services
         }
         public void Insert(Seller obj)
         {
-            obj.Department = _context.Department.First();
+            
             _context.Add(obj);
             _context.SaveChanges();
         }
