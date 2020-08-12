a6344bc8bce73aeb0c6132bb649829cf91fbecf4
gustavocapelini <gustavo.capelini8@gmail.com>
2020-08-12 18:02:18 -0300

    delete seller

--- SalesWebMvc/Models/Services/SellerService.cs
+++ SalesWebMvc/Models/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models.Services
@@ -20,9 +21,22 @@ namespace SalesWebMvc.Models.Services
         }
         public void Insert(Seller obj)
         {
-            
+
             _context.Add(obj);
             _context.SaveChanges();
         }
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
     }
 }
+
