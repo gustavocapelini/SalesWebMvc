ccc3d098c8b9063e20a5b953dd2fb6d697949a5e
gustavocapelini <gustavo.capelini8@gmail.com>
2020-08-12 18:45:34 -0300

    seller details and eager loading

--- SalesWebMvc/Models/Services/SellerService.cs
+++ SalesWebMvc/Models/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Models.Services
 {
@@ -27,7 +28,7 @@ namespace SalesWebMvc.Models.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
